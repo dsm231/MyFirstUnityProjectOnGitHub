db7610125eb8122226d10b3cef9e694173ac5810
dsm231 <deepmann.singh@gmail.com>
2021-10-13 23:30:22 +0530

    unity project

---
+++ MyFirstUnityProjectOngithub/Assets/scripts/sceneChanger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void loadSceneOne()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void loadSceneTwo()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void loadSceneThree()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void loadSceneMain()
+    {
+        SceneManager.LoadScene(0);
+       
+    }
+}
